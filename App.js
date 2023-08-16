abd1686462f3082e8ebb9b30a990575d42fed496
Ajay Rawat <60476050+theajayrawat@users.noreply.github.com>
2023-08-16 10:19:39 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const element1 = React.createElement(
+  "p",
+  { id: "para1", key: 1 },
+  "Hi i am Ajay Rawat"
+);
+const element2 = React.createElement(
+  "p",
+  { id: "para2", key: 2 },
+  "I am from Uttrakhand"
+);
+const container = React.createElement("div", { id: "container" }, [
+  element1,
+  element2,
+]);
+root.render(container);
