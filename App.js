1db9fb0a59ad44f22bd235961ef134d65a4eb2cd
Ajay Rawat <60476050+theajayrawat@users.noreply.github.com>
2023-08-16 13:00:30 +0530

    JSX

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const element1 = React.createElement(
-  "p",
-  { id: "para1", key: 1 },
-  "Hi i am Ajay Rawat"
+
+//React element
+const element1 = (
+  <p id="para1" key="1">
+    Hi 
+  </p>
 );
-const element2 = React.createElement(
-  "p",
-  { id: "para2", key: 2 },
-  "I am from Uttrakhand"
-);
-const container = React.createElement("div", { id: "container" }, [
-  element1,
-  element2,
-]);
-root.render(container);
+
+//React Component
+
+const FunctionalComponent = () =>{
+  var xyz=10;
+  return(
+    <div>
+      {element1}
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+root.render(<FunctionalComponent/>);
+
+
+
